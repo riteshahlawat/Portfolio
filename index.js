8ae8532fac79bee4edfb0f8fd6786ee9fbfbb9b1
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2020-03-27 02:00:29 -0400

    Update

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.enable("trust proxy");
 
 app.use(
   express.static(path.join(__dirname, "public"), {

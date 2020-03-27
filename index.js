efd3e45c3e63889de0fe9ae7a79bb586914ce531
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2020-03-27 01:59:40 -0400

    Update

--- index.js
+++ index.js
@@ -3,24 +3,32 @@ const path = require("path");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.enable("trust proxy");
 
 app.use(
-    express.static(path.join(__dirname, "public"), {
-        extensions: ["html"]
-    })
+  express.static(path.join(__dirname, "public"), {
+    extensions: ["html"]
+  })
 );
 
-app.get("*", function(request, response){
-    response.redirect("https://" + request.headers.host + request.url);
-    console.log("hawo");
-  });
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (
+    !req.secure &&
+    req.get("x-forwarded-proto") !== "https" &&
+    process.env.NODE_ENV !== "development"
+  ) {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+app.use(requireHTTPS);
 
-const PORT = 8787;
+const PORT = 443;
 
-app.listen(process.env.PORT || PORT, (err) => {
-    if (err) console.log
-    console.log(`Server started on port: ${PORT}`);
+app.listen(process.env.PORT || PORT, err => {
+  if (err) console.log;
+  console.log(`Server started on port: ${PORT}`);
 });

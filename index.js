be12d33a7bbf8ec78f5af7d3795666e77cc9ea95
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2020-03-27 01:55:01 -0400

    update

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.enable("trust proxy");
+
 app.use(
     express.static(path.join(__dirname, "public"), {
         extensions: ["html"]
     })
 );
 
-app.use((req, res) => {
-    if (!req.secure) {
-        response.redirect("https://" + request.headers.host + request.url);
-        console.log("Redirecting")
-    }
-})
+app.get("*", function(request, response){
+    response.redirect("https://" + request.headers.host + request.url);
+  });
 
 const PORT = 8787;
 

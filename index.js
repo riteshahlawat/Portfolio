3a04d618346aa858d9f5468898c0e1e8b430383b
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2020-04-05 02:06:19 -0400

    Update

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.get("/storeIp", (req, res) => {
   let lat = ipInfo.ll[0];
   let long = ipInfo.ll[1];
 
+  let message;
 
   let file;
   // Only parse if not your ip
@@ -51,47 +52,62 @@ app.get("/storeIp", (req, res) => {
     const messageEmbed = new Discord.MessageEmbed()
       .setColor("#fc0390")
       .setTitle("Someone Went On Your Website!")
-      .setAuthor("Ritesh Ahlawat", "https://ritesh.social/files/portfolio/me.jpg", "https://ritesh.social")
+      .setAuthor(
+        "Ritesh Ahlawat",
+        "https://ritesh.social/files/portfolio/me.jpg",
+        "https://ritesh.social"
+      )
       .setDescription("Damb bruh check this dumbass out yo")
-      .setThumbnail("https://iconsplace.com/wp-content/uploads/_icons/000000/256/png/letter-r-icon-256.png")
+      .setThumbnail(
+        "https://iconsplace.com/wp-content/uploads/_icons/000000/256/png/letter-r-icon-256.png"
+      )
       .addFields(
-        {name: "IP", value: `${tempIp}`, inline: true},
-        {name: "City", value: `${city}`, inline: true},
-        {name: "State", value: `${state}`, inline: true}, 
-        {name: "Country", value: `${country}`, inline: true},
-        {name: "Latitude", value: `${lat}`, inline: true},
-        {name: "Longitude", value: `${long}`, inline: true},
-        {name: '\u200B', value: '\u200B' }
+        { name: "IP", value: `${tempIp}`, inline: true },
+        { name: "City", value: `${city}`, inline: true },
+        { name: "State", value: `${state}`, inline: true },
+        { name: "Country", value: `${country}`, inline: true },
+        { name: "Latitude", value: `${lat}`, inline: true },
+        { name: "Longitude", value: `${long}`, inline: true },
+        { name: "\u200B", value: "\u200B" }
       )
       .setTimestamp()
       .setFooter("He do be getting yeeted doe");
-    
+
     // General channel
     logsChannel.send(messageEmbed);
     res.send(ipInfo);
   } else {
     message = `\nDamn bruh you just visited your own website dawg.\npwes enjoy this stupid fucking baby\n`;
     file = "https://i.ytimg.com/vi/AgmnUl31_ng/maxresdefault.jpg";
+    // General channel
     const messageEmbed = new Discord.MessageEmbed()
       .setColor("#fc0390")
-      .setTitle("Stupid Went On Your Website!")
-      .setAuthor("Ritesh Ahlawat", "https://ritesh.social/files/portfolio/me.jpg", "https://ritesh.social")
+      .setTitle("Someone Went On Your Website!")
+      .setAuthor(
+        "Ritesh Ahlawat",
+        "https://ritesh.social/files/portfolio/me.jpg",
+        "https://ritesh.social"
+      )
       .setDescription("Damb bruh check this dumbass out yo")
-      .setThumbnail("https://iconsplace.com/wp-content/uploads/_icons/000000/256/png/letter-r-icon-256.png")
+      .setThumbnail(
+        "https://iconsplace.com/wp-content/uploads/_icons/000000/256/png/letter-r-icon-256.png"
+      )
       .addFields(
-        {name: "IP", value: `${tempIp}`, inline: true},
-        {name: "City", value: `${city}`, inline: true},
-        {name: "State", value: `${state}`, inline: true}, 
-        {name: "Country", value: `${country}`, inline: true},
-        {name: "Latitude", value: `${lat}`, inline: true},
-        {name: "Longitude", value: `${long}`, inline: true},
-        {name: '\u200B', value: '\u200B' }
+        { name: "IP", value: `${tempIp}`, inline: true },
+        { name: "City", value: `${city}`, inline: true },
+        { name: "State", value: `${state}`, inline: true },
+        { name: "Country", value: `${country}`, inline: true },
+        { name: "Latitude", value: `${lat}`, inline: true },
+        { name: "Longitude", value: `${long}`, inline: true },
+        { name: "\u200B", value: "\u200B" }
       )
       .setTimestamp()
       .setFooter("He do be getting yeeted doe");
-      
-      logsChannel.send(messageEmbed);
-      res.send(ipInfo);
+
+    // General channel
+    logsChannel.send(messageEmbed);
+    res.send(ipInfo);
+  }
 });
 
 const PORT = 443;

